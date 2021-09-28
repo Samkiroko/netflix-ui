cf0c0f5b415db404370e4567e000aa53df230156
Samkiroko <kirokodev@gmail.com>
2021-09-28 17:20:55 +0300

    full dashboard design

---
+++ .history/src/home/Home_20210928171205.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './home.scss'
+
+const Home = () => {
+  return <div>hello</div>
+}
+
+export default Home
---
+++ .history/src/home/Home_20210928171217.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './home.scss'
+
+const Home = () => {
+  return <div className='home'>hello</div>
+}
+
+export default Home
