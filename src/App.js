79765af5b4bb79da5156a749f72b1ac073471854
Samkiroko <kirokodev@gmail.com>
2021-09-29 16:24:13 +0300

    login and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss'
 import Home from './components/pages/home/Home'
+import Login from './components/pages/login/Login'
+import Register from './components/pages/register/Register'
 import Watch from './components/pages/watch/Watch'
 
 function App() {
   return (
     <div className='App'>
       {/* <Home /> */}
-      <Watch />
+      {/* <Register /> */}
+      <Login />
     </div>
   )
 }
